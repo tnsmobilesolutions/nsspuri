2f7fd81125deb6bde74b7ebe004b0141c4f7198e
Amrutanshu <96327371+Amrutanshu-Chunu@users.noreply.github.com>
2023-10-10 00:03:25 +0530

    firebase implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'package:sammilani_delegate/authentication/signin_screen.dart';
+import 'package:sammilani_delegate/firebase_options.dart';
 import 'package:sammilani_delegate/theme/theme.dart';
 
 Future<void> main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: FirebaseOptions(apiKey: "AIzaSyDlUZaghUvp0OSayMgnYisIoSlAzKUBSAQ", appId: "1:29623966317:android:97bc27c64fab7a5f667d64", messagingSenderId: "", projectId: "nsspuridelegate")
-  // );
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
