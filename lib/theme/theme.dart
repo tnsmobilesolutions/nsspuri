b82826cd11ef58e7762990efb560b77a3cd6394f
tns <amrutanshup@gmail.com>
2023-10-16 23:54:26 +0530

    feature/home: added app bar in navigation bar.

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -5,20 +5,13 @@ const Color yellowClr = Color(0xFF9CBB2B);
 const Color pinkClr = Color(0xFFB742BB);
 const Color white = Color(0xFFFFFFFF);
 const primaryClr = Color(0xFFB4DA1B);
-const Color darkGreyClr  = Color(0xFF35353D);
+const Color darkGreyClr = Color(0xFF35353D);
 const Color darkHeaderClr = Color(0xFF161720);
 
-class Themes{
- static final light = ThemeData(
-        primaryColor: primaryClr,
-        brightness: Brightness.light
+class Themes {
+  static final light =
+      ThemeData(primaryColor: primaryClr, brightness: Brightness.light);
 
-       
-      );
-
-     static final dark = ThemeData(
-       primaryColor: darkGreyClr,
-        brightness: Brightness.dark 
-
-      );
+  static final dark =
+      ThemeData(primaryColor: darkGreyClr, brightness: Brightness.dark);
 }
