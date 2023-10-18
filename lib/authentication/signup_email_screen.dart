d16ba8e4616e5c0c7f686ab3780440db5454b69f
Amrutanshu-Chunu <amrutanshup@gmail.com>
2023-10-18 23:35:18 +0530

    sign up email

--- lib/authentication/signup_email_screen.dart
+++ lib/authentication/signup_email_screen.dart
@@ -30,6 +30,16 @@ class _SignupScreenState extends State<SignupScreen> {
     });
   }
 
+  void _toggleObscured1() {
+    setState(() {
+      _obscured = !_obscured;
+      if (textFieldFocusNode.hasPrimaryFocus)
+        return; // If focus is on text field, dont unfocus
+      textFieldFocusNode.canRequestFocus =
+          false; // Prevents focus if tap on eye
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -161,7 +171,7 @@ class _SignupScreenState extends State<SignupScreen> {
                   suffixIcon: Padding(
                     padding: const EdgeInsets.fromLTRB(0, 0, 4, 0),
                     child: GestureDetector(
-                      onTap: _toggleObscured,
+                      onTap: _toggleObscured1,
                       child: Icon(
                         _obscured
                             ? Icons.visibility_rounded
