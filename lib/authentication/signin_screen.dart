31f8885254fe3ecd2fd2bfdade53e72b7711def1
Amrutanshu-Chunu <amrutanshup@gmail.com>
2023-10-21 00:06:37 +0530

    signIn issue

--- lib/authentication/signin_screen.dart
+++ lib/authentication/signin_screen.dart
@@ -17,6 +17,7 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _passwordTextController = TextEditingController();
   final TextEditingController _emailTextController = TextEditingController();
   @override
@@ -26,87 +27,92 @@ class _SignInScreenState extends State<SignInScreen> {
         child: Padding(
           padding: EdgeInsets.fromLTRB(
               20, MediaQuery.of(context).size.height * 0.2, 20, 0),
-          child: Column(
-            children: <Widget>[
-              LogoWidget("assets/images/nsslogo.png"),
-              const SizedBox(
-                height: 30,
-              ),
-              ReusableTextField("Enter UserName", Icons.person_outline, false,
-                  _emailTextController),
-              const SizedBox(
-                height: 20,
-              ),
-              ReusableTextField("Enter Password", Icons.lock_outline, true,
-                  _passwordTextController),
-              const SizedBox(
-                height: 21,
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: 50,
-                margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
-                decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(90)),
-                child: ElevatedButton(
-                  onPressed: () async {
-                    try {
-                      final uid = await FirebaseAuthentication()
-                          .signinWithFirebase(_emailTextController.text,
-                              _passwordTextController.text);
-                      if (uid != null) {
-                        final data =
-                            await GetDevoteeAPI().loginDevotee(uid.toString());
-                        DevoteeModel devotee = data?["data"];
-                        if (devotee.uid == uid) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(content: Text('Signin Successful')));
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) {
-                                return HomePage(
-                                    uid: devotee.devoteeId.toString());
-                              },
-                            ),
-                          );
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                LogoWidget("assets/images/nsslogo.png"),
+                const SizedBox(
+                  height: 30,
+                ),
+                ReusableTextField("Enter UserName", Icons.person_outline, false,
+                    _emailTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                ReusableTextField("Enter Password", Icons.lock_outline, true,
+                    _passwordTextController),
+                const SizedBox(
+                  height: 21,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 50,
+                  margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(90)),
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      try {
+                        final uid = await FirebaseAuthentication()
+                            .signinWithFirebase(_emailTextController.text,
+                                _passwordTextController.text);
+                        if (uid != null) {
+                          final data = await GetDevoteeAPI()
+                              .loginDevotee(uid.toString());
+                          DevoteeModel devotee = data?["data"];
+                          if (devotee.uid == uid) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text('Signin Successful')));
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return HomePage(uid: devotee.uid.toString());
+                                },
+                              ),
+                            );
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(content: Text('Signin failed')));
+                          }
                         } else {
                           ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(content: Text('Signin failed')));
+                              const SnackBar(
+                                  content: Text(
+                                      'User does not exist. Please sign up.')));
                         }
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                            content:
-                                Text('User does not exist. Please sign up.')));
+                      } catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text(e.toString())));
                       }
-                    } catch (e) {
-                      ScaffoldMessenger.of(context)
-                          .showSnackBar(SnackBar(content: Text(e.toString())));
-                    }
-                  },
-                  child: const Text(
-                    "Sign In",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16),
-                  ),
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.resolveWith((states) {
-                        if (states.contains(MaterialState.pressed)) {
+                    },
+                    child: const Text(
+                      "Sign In",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    ),
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.resolveWith((states) {
+                          if (states.contains(MaterialState.pressed)) {
+                            return Colors.deepOrange;
+                          }
                           return Colors.deepOrange;
-                        }
-                        return Colors.deepOrange;
-                      }),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(90)))),
+                        }),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(90)))),
+                  ),
                 ),
-              ),
-              forgetPassword(context),
-              signUpOption()
-            ],
+                forgetPassword(context),
+                signUpOption()
+              ],
+            ),
           ),
         ),
       ),
