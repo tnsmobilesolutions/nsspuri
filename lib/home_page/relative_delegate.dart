9eea52090e013eaa7802c20a022b3f192327a3af
Dhananjay Moharana <csdhananjaymoharana@gmail.com>
2023-12-21 19:50:37 +0530

    version updated and color change

--- lib/home_page/relative_delegate.dart
+++ lib/home_page/relative_delegate.dart
@@ -33,7 +33,7 @@ class _RelativeDelegateState extends State<RelativeDelegate> {
     if (devotee.isGuest == true) {
       return Colors.yellow;
     } else if (devotee.isOrganizer == true) {
-      return Colors.red;
+      return Color.fromARGB(255, 220, 31, 18);
     } else if (devotee.dob != "" && devotee.dob != null) {
       if (isValidDateFormat(devotee.dob.toString())) {
         if (devotee.isSpeciallyAbled == true ||
@@ -50,7 +50,7 @@ class _RelativeDelegateState extends State<RelativeDelegate> {
     } else if (devotee.gender == "Male") {
       return Colors.blue;
     } else if (devotee.gender == "Female") {
-      return Colors.pink;
+      return Color.fromARGB(255, 254, 117, 163);
     } else {
       return Colors.blue;
     }
